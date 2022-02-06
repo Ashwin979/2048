34b5b9b87b6cc8fa24740228b0e58f11b92edf27
Ashwin979 <ashwin.psna@gmail.com>
2022-02-06 11:35:39 +0530

    control updates

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import copy 
 import random
 
 boardsize = 4
+def display():
+    largest = board[0][0]
+    for row in board:
+        for num in row:
+            if num > largest:
+                largest = num
+    numspaces = len(str(largest))
+    for row in board:
+        output = "|"
+        for num in row:
+            if num == 0:
+                output += " " * numspaces + "|"
+            else:
+                output += (" " * (numspaces - len(str(num)))) + str(num) + "|"
+        print(output)
+    print()
+
 def mergeonerowleft(row):
     for j in range(boardsize -1):
         for i in range(boardsize -1, 0, -1):
@@ -63,6 +81,18 @@ def chooseavalue():
      else:
         return 2
 
+def addnewvalue():
+    rownum = random.randint(0, boardsize - 1)
+    colnum = random.randint(0, boardsize - 1)
+
+    while not board[rownum][colnum] == 0:
+        rownum = random.randint(0, boardsize - 1)
+        colnum = random.randint(0, boardsize - 1)
+    
+    board[rownum][colnum] = chooseavalue()
+
+
+    
 board = []
 for i in range(boardsize):
     row = []
@@ -79,23 +109,33 @@ while numwanted > 0:
        board[rownum][colnum] = chooseavalue()
        numwanted -= 1
 
-print("HAI!! Welcome to 2048 your goal is to combine values to get the number 2048,by merging the board in differnet directions.Each and every time you will need to type'2' to merge right ,'1' to merge left,'3' to merge up, and '4' to merge down.\n\n Here is the starting board: ")
-
-
-def display():
-    largest = board[0][0]
-    for row in board:
-        for num in row:
-            if num > largest:
-                largest = num
-    numspaces = len(str(largest))
-    for row in board:
-        output = "|"
-        for num in row:
-            if num == 0:
-                output += " " * numspaces + "|"
-            else:
-                output += (" " * (numspaces - len(str(num)))) + str(num) + "|"
-        print(output)
-    print()
+print("HAI!! Welcome to 2048 your goal is to combine values to get the number 2048,by merging the board in differnet directions.Each and every time you will need to type'1' to merge left ,'2' to merge right,'3' to merge up, and '4' to merge down.\n Here is the starting board: ")
 display()
+
+gameover = False
+
+while not gameover:
+    move=input("which way do you want to merge")
+    
+    validinput = True
+    tempboard = copy.deepcopy(board)
+
+    if move == "1":
+        board = merge_left(board)
+    elif move == "2":
+        board = merge_right(board)
+    elif move == "3":
+        board = merge_up(board)
+    elif move == "4":
+        board = merge_down(board)
+    else:
+        validinput = False
+
+    if not validinput:
+        print("your input was not valid,please try again by entering a valid input")
+    else:
+        if board == tempboard:
+            print("try a different direction!!!")
+        else:
+            addnewvalue()
+            display()
