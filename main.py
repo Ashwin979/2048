d286db72ccb3fe7c835661c7f87e8dc2d203eaf1
Ashwin979 <ashwin.psna@gmail.com>
2022-02-06 10:54:00 +0530

    game starting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-board =[[0,0,0,0],[2,2,2,2],[4,4,2,2],[0,2,0,2]]
+import random
+
 boardsize = 4
 def mergeonerowleft(row):
     for j in range(boardsize -1):
@@ -56,6 +57,31 @@ def merge_down(currentboard):
     currentboard = transpose(currentboard)
     return currentboard
 
+def chooseavalue():
+     if random.randint(1,8) == 1:
+        return 4
+     else:
+        return 2
+
+board = []
+for i in range(boardsize):
+    row = []
+    for j in range(boardsize):
+       row.append(0)
+    board.append(row)
+
+numwanted = 2
+while numwanted > 0:
+   rownum = random.randint(0, boardsize - 1)
+   colnum = random.randint(0, boardsize - 1)
+
+   if board[rownum][colnum] == 0:
+       board[rownum][colnum] = chooseavalue()
+       numwanted -= 1
+
+print("HAI!! Welcome to 2048 your goal is to combine values to get the number 2048,by merging the board in differnet directions.Each and every time you will need to type'2' to merge right ,'1' to merge left,'3' to merge up, and '4' to merge down.\n\n Here is the starting board: ")
+
+
 def display():
     largest = board[0][0]
     for row in board:
@@ -72,8 +98,4 @@ def display():
                 output += (" " * (numspaces - len(str(num)))) + str(num) + "|"
         print(output)
     print()
-display()
-merge_up(board)
-display()
-merge_down(board)
 display()
